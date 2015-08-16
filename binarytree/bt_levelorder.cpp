6a40b63fa0e7df90a6d865575208b090461d1399
tainanboy <frank840925@gmail.com>
2015-08-16 21:21:54 +0800

    8/16/2015 add solution of Binary Tree Level Order Traversal
    Using two functions, one use 2D array to get data(for level order print
    out), the other to get the height of binary tree.

---
+++ binarytree/bt_levelorder.cpp
@@ -0,0 +1,34 @@
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+
+ class Solution {
+ public:
+     vector<vector<int>> levelOrder(TreeNode* root) {
+       int depth=getheight(root);
+       vector<vector<int>> tree(depth);
+       if(depth==0) return tree;
+       getdata(tree,root,0);
+       return tree;
+     }
+     void getdata(vector<vector<int>>& tree, TreeNode* root, int level){
+     if(root==NULL)
+         return;
+     tree[level].push_back(root->val);
+     getdata(tree,root->left,level+1);
+     getdata(tree,root->right,level+1);
+ }
+
+     int getheight(TreeNode* root){
+       if(root==NULL) return 0;
+       int left=getheight(root->left);
+       int right=getheight(root->right);
+       int height=(left>right?left:right)+1;
+       return height;
+     }
+ };
