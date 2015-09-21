63afc4fc66d3b31e1a4805a701bbdc0e9fb0a2cb
tainanboy <frank840925@gmail.com>
2015-09-21 23:36:13 +0800

    Solution to Move Zeros
    20ms

---
+++ Move zeros/Move_Zeroes.cpp
@@ -0,0 +1,12 @@
+//Move Zeroes
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+      int cur=0;
+      for(int i=0;i<nums.size();i++){
+        if(nums[i]!=0){
+          swap(nums[i],nums[cur++]);
+        }
+      }
+  }
+};
